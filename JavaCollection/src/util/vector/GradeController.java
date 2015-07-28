712633d4849dc9899d516a660d81c6bdfe20b355
평일반 <@ITBANK28-PC>
2015-07-28 15:25:32 +0900

    2015.07.28

--- JavaCollection/src/util/vector/GradeController.java
+++ JavaCollection/src/util/vector/GradeController.java
@@ -1,5 +1,7 @@
 package util.vector;
 
+import java.util.Scanner;
+
 public class GradeController {
 	public static void main(String[] args) {
 		GradeService service = new GradeServiceImpl();
@@ -11,7 +13,79 @@ public class GradeController {
 		service.input(hong);
 		service.input(kim);
 		service.input(lee);
-		// 전체 학생 성적표를 출력
+		
 		service.pintList();
+		/*지시값을 전달할 스캐너*/
+		Scanner scanner = new Scanner(System.in);
+	 	int input = 0, key = 0;
+	 	/*무한루프 엔진*/
+	 	while (true) {
+	 		/*메뉴판*/
+	 		System.out.println("1.성적입력 2. 성적조회(학번) 3. 성적조회(이름) 4. 전체성적 5. 성적순 6. 이름순 0. 종료");
+	 		input = scanner.nextInt();
+	 		/*입력값 체크 (밸류데이션)*/
+	 		key = inputCheck(input, 0, 6);
+	 		if (input !=0) {
+	 			switch (key) {
+	 			case 1: /*성적입력*/
+	 				System.out.println("학번 :");
+	 				String hakbun = scanner.next();
+	 				System.out.println("이름");
+	 				String name = scanner.next();
+	 				System.out.println("국어 :");
+	 				int kor = scanner.nextInt();
+	 				System.out.println("영어 :");
+	 				int eng = scanner.nextInt();
+	 				System.out.println("수학 :");
+	 				int math = scanner.nextInt();
+	 				Grade grade = new Grade(hakbun, name, kor, eng, math);
+	 				service.input(grade);
+	 				break;
+	 			case 2: /*2. 성적조회(학번)*/
+	 				System.out.println("학번 :");
+	 				System.out.println(service.searchGradeByhakbun(scanner.next()));
+	 				break;
+	 			case 3: /*3. 성적조회(이름)*/
+	 				System.out.println("이름 :");
+	 				System.out.println(service.searchGradeByName(scanner.next()));
+	 				break;
+	 			case 4: /*4. 전체성적*/
+	 				break;
+	 			case 5: /*5. 성적순*/
+	 				service.decsByTotal();
+	 				break;
+	 			case 6: /*6. 이름순*/
+	 				service.ascByName();
+	 				break;
+	 			default: System.out.println("에러발생");
+	 				break;
+	 			}
+	 		}
+	 		
 	}
+	
+	
+	
+	
+}
+	private static int inputCheck(int input, int i, int j) {
+		if (input < i || input > j) {
+			System.out.println("선택메뉴에서 범위값 외의 수를 입력");
+			return 0; // 시스템 조욜.. 리턴타입의 디폴트값을 리턴화
+		} else {
+			return input;
+		}
+	
+	}
+/*
+인터페이스구현한 객체 + 스캐너 + 인풋변수 ..장착
+
+while(true){
+	if(input != 0){
+	
+	}else{
+	
+	}
+}
+*/
 }
--- JavaCollection/src/util/vector/GradeServiceImpl.java
+++ JavaCollection/src/util/vector/GradeServiceImpl.java
@@ -1,6 +1,12 @@
 package util.vector;
-
+// 전체에서 특정 글자 바꾸기 단축키 CTRL + F 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Vector;
+
+import sun.nio.cs.ext.GB18030;
 /*
 프로그램을 코딩하다 보면 자료구조를 다루는데 있어서 
 CRUD 액션의 반복을 경험하게 됩니다.
@@ -14,7 +20,7 @@ public class GradeServiceImpl implements GradeService{
 	// 필드에 아래 메소드들이 공유할 자료구조를 뭘 쓸까?
 	// 1. ArrayList 2. Vector 3. Stack 4. HashMap
 	// 순서 0, 중복 0
-	private Vector<Grade> vec = new Vector<Grade>(); 
+	private List<Grade> vec = new ArrayList<Grade>(); 
 	Grade grade = new Grade(); // ㅇ디폴트 생성자가 있어서 에러 제거
 	
 	@Override
@@ -39,16 +45,16 @@ public class GradeServiceImpl implements GradeService{
 		// List 계열의 클래스 길이는 size() 구한다.
 		for (int i = 0; i < vec.size(); i++) {
 			// 만약 vec가 배열이라면 vec.get(i)
-			String searchHakbun = vec.elementAt(i).getHakbun();
+			String searchHakbun = vec.get(i).getHakbun();
 			// 객체.메소드.메소드.메소드.메소드 이런
 			// 패턴이 가능할 때는 반드시 return 값이 있는 메소드들 끼리 연결시에만 가능하다
 			// 이런 방식을 메소드 체인이라고함
 				
 			if (hakbun.equalsIgnoreCase(searchHakbun)) {
-			String name = vec.elementAt(i).getName();
-			int kor = vec.elementAt(i).getKor();
-			int eng = vec.elementAt(i).getEng();
-			int math = vec.elementAt(i).getMath();
+			String name = vec.get(i).getName();
+			int kor = vec.get(i).getKor();
+			int eng = vec.get(i).getEng();
+			int math = vec.get(i).getMath();
 			grade = new Grade(searchHakbun,name,kor,eng,math);
 			msg = grade.toString();
 			break; // 중간이라도 학번이 일치하면 그대로 스톱
@@ -59,10 +65,84 @@ public class GradeServiceImpl implements GradeService{
 		return msg;
 	}
 
+	
 	@Override
-	public void ascGradeTotal() {
-		// 성적 정렬
+	public Vector<Grade> searchGradeByName(String name) {
+		Vector<Grade> temp = new Vector<Grade>();
+		Grade grade = null;
+		for (int i = 0; i < vec.size(); i++) {
+			String sarchName = vec.get(i).getName();
+			// 고정값(파라미터).equals(변수값)
+			if (name.equalsIgnoreCase(sarchName)) {
+				grade = new Grade(vec.get(i).getHakbun(), 
+						name, 
+						vec.get(i).getKor(), 
+						vec.get(i).getEng(), 
+						vec.get(i).getMath());
+				temp.add(grade);
+				
+			} else {
+				// temp라는 벡터를 완전히 비워서 null로 리턴
+				temp.remove(new Grade());
+			}
+			
+		}
+		return temp;
+	}
+
+	@Override
+	public void decsByTotal() {
+		// java API중에 정렬담당 클래스 
+		// Comparator
+		// 인터페이스를 구현한 익명 내부 클래스
+		// anonymous inner class
+		Comparator<Grade> desc = new Comparator<Grade>(){ 
 		
+				
+			@Override
+			public int compare(Grade g1, Grade g2) {
+				// 삼항연산자
+				/*
+				 if(condition){
+				 	--> true면 statement 실행
+				 }else{
+				 	--> false면 statement 실행
+				 }
+				 (조건식) ? 참 : 거짓;
+				 */
+				
+				/*if ((g1.getTotal()<g2.getTotal())) {
+					temp = 1;
+				} else {
+					if (g1.getTotal()==g2.getTotal()) {
+						temp = 0;
+					} else {
+						temp = -1;
+					}
+				}
+				return temp;
+				*/
+				return (g1.getTotal()<g2.getTotal()) ? 1 : 
+					   (g1.getTotal()==g2.getTotal()) ? 0 : -1 ;
+			}
+		};
+		Collections.sort(vec,desc);
+		System.out.println(vec.toString());
+	}
+
+	@Override
+	public void ascByName() {
+		Comparator<Grade> asc = new Comparator<Grade>() {
+			
+			@Override
+			public int compare(Grade o1, Grade o2) {
+				// 값(value)이 int타입이 아니고 String 타입의 우선순위 결정할 때는...
+				// compareTo() 메소드를 사용해야한다
+				return o1.getName().compareTo(o2.getName());
+			}
+		};
+		Collections.sort(vec, asc);
+		System.out.println(vec.toString());
 	}
 
 }
